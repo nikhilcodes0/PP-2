9a404be9f0622a6362b35c39d100c02eeda690be
Nikhil Kumar <nk748577732@gmail.com>
2023-03-09 20:01:11 +0530

    Added Tap to Top button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 const icon = document.getElementsByClassName("ham")[0];
+const toTop = document.getElementsByClassName("to_top")[0];
 icon.addEventListener('click', () => {
   icon.classList.toggle("open");
 });
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    toTop.classList.add("display");
+  }
+  else {
+    toTop.classList.remove("display");
+  }
+}
