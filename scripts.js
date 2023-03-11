47707c6365388973a3e856199a1b19662c1d97ee
Nikhil Kumar <nk748577732@gmail.com>
2023-03-11 17:59:23 +0530

    Added Scroll to Reveal Sections

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 const icon = document.getElementsByClassName("ham")[0];
 const toTop = document.getElementsByClassName("to_top")[0];
+const reveal = document.getElementsByClassName("reveal");
 icon.addEventListener('click', () => {
   icon.classList.toggle("open");
 });
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(), scrollReveal()};
 
 function scrollFunction() {
   if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -12,5 +13,20 @@ function scrollFunction() {
   }
   else {
     toTop.classList.remove("display");
+  }
+}
+
+function scrollReveal() {
+  for (var i = 0; i < reveal.length; i++) {
+    const revealWindow = window.innerHeight;
+    const revealTop = reveal[i].getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < revealWindow - revealPoint) {
+      reveal[i].classList.add("active");
+    }
+    else {
+      reveal[i].classList.remove("active");
+    }
   }
 }
