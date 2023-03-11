a4e4481cfb36a7b0c72c35228a7d6dc3d223c4f9
Nikhil Kumar <nk748577732@gmail.com>
2023-03-11 18:16:07 +0530

    Added some avbar Animations

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 const icon = document.getElementsByClassName("ham")[0];
 const toTop = document.getElementsByClassName("to_top")[0];
 const reveal = document.getElementsByClassName("reveal");
+const nav = document.getElementsByClassName("navbar")[0];
+const logo = document.getElementsByClassName("logo")[0];
+const l2 = document.getElementsByClassName("line2")[0];
+
 icon.addEventListener('click', () => {
   icon.classList.toggle("open");
 });
@@ -10,9 +14,18 @@ window.onscroll = function() {scrollFunction(), scrollReveal()};
 function scrollFunction() {
   if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     toTop.classList.add("display");
+    nav.classList.add("shrink");
+    logo.classList.add("shrink");
+    icon.classList.add("shrink");
+    l2.classList.add("shrink");
+
   }
   else {
     toTop.classList.remove("display");
+    nav.classList.remove("shrink");
+    logo.classList.remove("shrink");
+    icon.classList.remove("shrink");
+    l2.classList.remove("shrink");
   }
 }
 
